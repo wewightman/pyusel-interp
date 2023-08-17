d17e4060f6b13bf7999511fa1be79c9b3bd40a67
Wren Wightman <wren.wightman@duke.edu>
2023-08-17 16:28:13 -0400

    Got buffer interpolator working

--- examples/test_set_interp.py
+++ examples/test_set_interp.py
@@ -1,8 +1,8 @@
 import numpy as np
-from interp.engines import IntCub1DSet
+from interp.engines import IntCub1DSet, IntCub1DSet_Buffer
 import matplotlib.pyplot as plt
 from time import time
-
+from cinpy import copy2c, copy2py, free
 
 M = int(64) # number of elements
 N = int(512) # number of samples
@@ -12,9 +12,9 @@ T = 64
 y0 = np.ones((M,1)) * np.sin(2*np.pi*np.arange(N)/T).reshape((1,N))
 
 t0 = time()
-fset = IntCub1DSet(M, N)
+fset0 = IntCub1DSet(M, N)
 t1 = time()
-fset.fill(y0)
+fset0.fill(y0)
 t2 = time()
 
 psel = (N-P+1)//2
@@ -22,10 +22,29 @@ psel = psel + P * np.sin(2*np.pi*np.arange(M)/M).reshape((M,1))
 psel = psel + np.arange(P).reshape((1,P))
 
 t3 = time()
-y1 = fset(psel)
+y1 = fset0(psel)
 t4 = time()
 
 print(1E6*(t1-t0), 1E6*(t2-t1), 1E6*(t4-t3))
+
+
+py0, cM, cN = copy2c(y0)
+
+t0 = time()
+fset1 = IntCub1DSet_Buffer(M, N)
+t1 = time()
+fset1.fill(py0, cM, cN)
+t2 = time()
+
+ppsel, cM, cP = copy2c(psel)
+
+t3 = time()
+py2 = fset1(ppsel, cM, cP)
+t4 = time()
+
+print(1E6*(t1-t0), 1E6*(t2-t1), 1E6*(t4-t3))
+
+y2 = copy2py(py2, cM, cP)
 
 plt.figure()
 plt.imshow(y0, aspect=N/M)
@@ -35,6 +54,12 @@ plt.colorbar()
 
 plt.figure()
 plt.imshow(y1, aspect=N/M)
+plt.ylabel("Element Index in set")
+plt.xlabel("Sample Index")
+plt.colorbar()
+
+plt.figure()
+plt.imshow(y2, aspect=N/M)
 plt.ylabel("Element Index in set")
 plt.xlabel("Sample Index")
 plt.colorbar()
