f35887ee72b1638c423d0728f215d5e16068f7a1
Wren Wightman <wren.wightman@gmail.com>
2023-09-29 01:14:37 -0400

    working cubic 1D with generalized tensor types

--- examples/test_set_interp.py
+++ examples/test_set_interp.py
@@ -2,22 +2,23 @@ import numpy as np
 from interp.engines import IntCub1DSet
 import matplotlib.pyplot as plt
 from time import time
-from cinpy import copy2c, copy2py, free
+from cinpy.types import CDataTensor
 
 M = int(64) # number of elements
 N = int(512) # number of samples
-P = int(256)
+P = int(64)
 T = 64
 
 y0 = np.ones((M,1)) * np.sin(2*np.pi*np.arange(N)/T).reshape((1,N))
+
+y0 = CDataTensor.fromnumpy(y0)
 
 t0 = time()
 fset0 = IntCub1DSet(y0)
 t1 = time()
 
-tausel = (N-P+1)//2
-tausel = tausel + P * np.sin(2*np.pi*np.arange(M)/M).reshape((M,1))
-isel = np.arange(P).reshape((1,P))
+tausel = P * np.sin(2*np.pi*np.arange(M)/M)
+isel = np.arange(N)
 
 t3 = time()
 y1 = fset0(tausel, isel)
@@ -25,15 +26,18 @@ t4 = time()
 
 print(1E6*(t1-t0), 1E6*(t4-t3))
 
+print(y0, y1)
+
 plt.figure()
-plt.imshow(y0, aspect=N/M)
+plt.imshow(y0.copy2np(), aspect=N/M)
 plt.ylabel("Element Index in set")
 plt.xlabel("Sample Index")
 plt.colorbar()
 
 plt.figure()
-plt.imshow(y1, aspect=N/M)
+plt.imshow(y1.copy2np(), aspect=N/M)
 plt.ylabel("Element Index in set")
 plt.xlabel("Sample Index")
 plt.colorbar()
 
+plt.show()
--- interp/engines/cubic1d.py
+++ interp/engines/cubic1d.py
@@ -1,7 +1,8 @@
 """Python wrapper for c-based 1D cubic interpolators"""
 import ctypes as ct
 import numpy as np
-from cinpy import copy2c, copy2py, free
+from cinpy.types import CDataTensor, DataTensor
+from cinpy import copy2c, free
 from glob import glob
 import platform as _pltfm
 import os
@@ -79,102 +80,81 @@ __cubic1d__.free_IntrpData1D_Fixed.argtypes = (ct.POINTER(ct.POINTER(Knots1D_Fix
 __cubic1d__.free_IntrpData1D_Fixed.restype = (None)
 __cubic1d__.free_IntrpData1D_Fixed.__doc__ = """Free a given knot structure"""
 
+def __make_knot_recursive__(data:CDataTensor, dn, n0, fill):
+    if data.isvec():
+        dtype = data.dtype
 
-class IntCub1DSet():
-    def __init__(self, mat:np.ndarray, dn=1, n0=0, fill=0):
-        """Build a flexible interpolator set for M, length-N vectors
-        
-        Returns a callable interpolator set that can handle M vectors of length N
-        """
-        M, N = mat.shape
-        self.knots = None
-        self.M = M
-        self.N = N
-
-        self.fill(mat, dn, n0, fill)
-
-    def clean(self):
-        """Remove data loaded into """
-        if self.knots is None:
-            return
-        
-        # clear this set's knots
-        knots = self.knots
-        self.knots = None
-        for knot in knots:
-            __cubic1d__.free_IntrpData1D_Fixed(ct.byref(knot))
-
-    def fill(self, mat:np.ndarray, dn=1, n0=0, fill=0):
-        """Take an M by N matrix and generate knots for it"""
-
-        # Validate input matrix
-        if (mat.shape[0] != self.M) or (mat.shape[1] != self.N):
-            raise ValueError(f"Dimensions were supposed to be ({self.M}, {self.N}) but were ({mat.shape[0]}, {mat.shape[1]})")
-        
-        # convert input matrix to set of pointers
-        pmat, cM, cN = copy2c(mat)
+        if dtype != ct.c_float: raise ValueError("Data must be stored as a float for now")
 
         # define consistent inputs
-        c_dn = ct.c_float(dn)
-        c_n0 = ct.c_float(n0)
-        c_Ny = ct.c_int(self.N)
-        c_fill = ct.c_float(fill)
+        c_dn = dtype(dn)
+        c_n0 = dtype(n0)
+        c_Ny = ct.c_int(data.shape[0])
+        c_fill = dtype(fill)
         c_copy = ct.c_int(1)
 
         # for each vector...
-        self.knots = []
-        for m in range(self.M):
-            # ...build an interpolator
-            py = pmat[m]
-            knot = __cubic1d__.tie_knots1D_fixed(ct.byref(py), c_Ny, c_dn, c_n0, c_fill, c_copy)
-            self.knots.append(knot)
-
-        # clean the numpy array buffer
-        free(pmat, cM, cN)
-
-    def __call__(self, taus:np.ndarray, points:np.ndarray|None=None, asbuffer=False):
-        """Extract estimates of the input functions at timepoint taus
-        
-        Parameters:
-        ----
-        taus: a M rows of intputs, can be 1D or 2D. Second dimension (T) indicates the number of different delay tabs to consider
-        points: vector of additive values to consider for each delay tab"""
-        if self.knots is None:
-            raise ValueError("Class of IntCub1DSet must be filled with data")
-        
-        if np.ndim(taus) == 1:
-            if (len(taus) != self.M):
-                raise ValueError(f"Dimensions were supposed to be ({self.M}) but were ({taus.shape[0]})")
-            # make points 2D if it is a vector
-            taus = taus.reshape((self.M,1,1))
-            T = 1
-
-        # get T reps 
-        elif np.ndim(taus) == 2:
-            if (taus.shape[0] != self.M):
-                raise ValueError(f"Dimensions were supposed to be ({self.M}) but were ({taus.shape[0]})")
-            taus = taus.reshape((self.M,-1,1))
-            T = taus.shape[1]
-        else:
-            raise ValueError(f"Points must be a length-{self.M} vector or have dimensions of ({self.M}, P)")
-        
-        # broadcast points
-        if points is not None:
-            taus = taus * points.reshape((1, 1, -1))
-
-        c_out = (ct.POINTER(ct.POINTER(ct.c_float) * T) * self.M)()
-        c_out = ct.cast(c_out, ct.POINTER(ct.POINTER(ct.c_float)))
-
-        # interpolate each axis for each value of ppoints
-        for ip in range(self.M):
-            
-            for it in range(T):
-                psel = ppoints[ip]
-                c_out[ip] = __cubic1d__.cubic1D_fixed(ct.byref(psel), cP, ct.byref(self.knots[ip]))
-        
-        # convert c_out to numpy array
-        out = copy2py(c_out, M=self.M, N=cP)
-        return out
+        return __cubic1d__.tie_knots1D_fixed(data.byref(), c_Ny, c_dn, c_n0, c_fill, c_copy)
+    else:
+        buffer = []
+        for subdata in data:
+            buffer.append(__make_knot_recursive__(subdata, dn, n0, fill))
+        return buffer
     
+def __select_knot_recursive__(knots, data:CDataTensor):
+    if not isinstance(knots, list):
+        return __sample_knot_recursive__(knots, data)
+    else:
+        selected = [__select_knot_recursive__(subknots, subdata).pntr for subknots, subdata in zip(knots, data)]
+        arr = ((data[0].getctype()) * len(selected))(*selected)
+        pntr = ct.cast(arr, data.getctype())
+        return CDataTensor(pntr, (len(selected), *(data.shape[1:])), data.dtype)
+
+def __sample_knot_recursive__(knot, data:CDataTensor):
+    if data.isvec():
+        dtype = data.dtype
+        # define consistent inputs
+        N = ct.c_int(data.shape[0])
+        # for each vector...
+        return CDataTensor(__cubic1d__.cubic1D_fixed(data.byref(), N, ct.byref(knot)), (N.value,), data.dtype)
+    
+    else:
+        selected = [__sample_knot_recursive__(knot, subdata).pntr for subdata in data]
+        arr = ((data[0].getctype()) * len(selected))(*selected)
+        pntr = ct.cast(arr, data.getctype())
+        return CDataTensor(pntr, (len(selected), *(data.shape[1:])), data.dtype)
+    
+class IntCub1DSet():
+    def __init__(self, data:CDataTensor, dn=1, n0=0, fill=0):
+        self.data = data
+        self.knots_shape = data.shape[:-1]
+        self.Nknots = np.prod(self.knots_shape)
+        self.knots = __make_knot_recursive__(data, dn, n0, fill)
+
     def __del__(self):
-        self.clean()
+        for knot in self.knots:
+            __cubic1d__.free_IntrpData1D_Fixed(ct.byref(knot))
+
+    def __call__(self, taus, points=None):
+        # validate that taus has at least the same shape 
+        wrongshape = ValueError("Taus must have at least the same dimensions as data.shape[:-1]")
+        if (len(self.knots_shape) > len(taus.shape)):
+            raise wrongshape
+        else: 
+            for dimk, dimt in zip(self.knots_shape, taus.shape): 
+                if (dimk != dimt): raise wrongshape 
+
+        tshape = taus.shape
+        kshape = self.knots_shape
+
+        if points is None:
+            raise NotImplementedError("Get good ho")
+        else:
+            taus = np.expand_dims(taus, ([int(len(taus.shape) + val) for val in np.arange(len(points.shape))]))
+            points = np.expand_dims(points, ([int(val) for val in np.arange(len(tshape))]))
+            tauprime = taus + points
+            tauten = CDataTensor.fromnumpy(tauprime, self.data.dtype)
+            return __select_knot_recursive__(self.knots, tauten)
+            
+
+                
